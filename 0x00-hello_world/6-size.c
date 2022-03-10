bfbb1d9a820c2e7768503cc5161c027086af0bcf
Middo <hamidadeshina1@gmail.com>
2022-03-10 15:55:01 +0100

    This is really nice

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,9 +17,9 @@ int sc = sizeof(c);
 int sd = sizeof(d);
 int se = sizeof(e);
 
-printf("%d", sa);
-printf("%d", sb);
-printf("%d", sc);
-printf("%d", sd);
-printf("%d", se);
+printf("Size of a char: %d byte(s)\n", sa);
+printf("Size of an int: %d byte(s)\n", sb);
+printf("Size of a long int: %d byte(s)\n", sc);
+printf("Size of a long long int: %d byte(s)\n", sd);
+printf("Size of a float: %d byte(s)\n", se);
 }
