0cfed1c98d9b65666a1753c3e3f68bada6bac786
Middo <hamidadeshina1@gmail.com>
2022-03-10 14:07:41 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,7 @@
 * main - print the phrase "Programming is like building a multilingual puzzle
 * Return: 0 of exited properly, non-zero otherwise
 */
-
-
-int main (void)
+int main(void)
 {
      puts ("\"Programming is like building a multilingual puzzle");
      return (0);
