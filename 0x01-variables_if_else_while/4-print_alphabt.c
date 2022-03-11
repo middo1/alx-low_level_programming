486b2f46b4f1103a27ea56da55157eab6bc4edae
middo1 <hamidadeshina1@gmail.com>
2022-03-11 08:25:28 -0800

    yep

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,7 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		if (ch == 'e' || ch == 'q')
-			putchar('');
-		else
+		if (ch != 'e' && ch != 'q')
 			putchar(ch);
 	putchar('\n');
 	return (0);
