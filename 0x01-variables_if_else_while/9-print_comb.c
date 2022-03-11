c3c80936af609b15a7c57a849a37ba7bfeb03440
middo1 <hamidadeshina1@gmail.com>
2022-03-11 09:57:22 -0800

    yep

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,11 @@ int main (void)
 {
 	int n;
 
-	putchar(n);
-	if (n != 57)
-		putchar(',');
-		putchar(' ');
+	for (n = 48; n <= 57 ; n++)
+		putchar(n);
+		if (n != 57)
+			putchar(',');
+			putchar(' ');
 	putchar('\n');
 	return (0);
 }
