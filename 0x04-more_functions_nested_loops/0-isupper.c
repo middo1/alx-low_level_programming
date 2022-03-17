e9a4b387dc06fa0766a9a67476ab1c30cfecda59
middo1 <hamidadeshina1@gmail.com>
2022-03-17 08:06:45 -0700

    lets gooo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * Return: 1 (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
+
