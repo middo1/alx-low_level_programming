b65d01a3de59632fe28358b717b260c38313cf8b
middo1 <hamidadeshina1@gmail.com>
2022-03-17 10:40:02 -0700

    ment or malaria

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_line - Entry point
- * @n - the number of times that '_' is printed
+ * @n - the number of times that _ is printed
  */
 void print_line(int n)
-{
-	int m;
-	
+{	
 	if (n > 0)
 	{
+		int m;
+
 		for (m = 1 ; m <= n ; m++ )
 		{
 			_putchar('_');
