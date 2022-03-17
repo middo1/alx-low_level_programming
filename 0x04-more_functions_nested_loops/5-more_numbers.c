c4b91aa54ec26dba1deef8aed3d3c4d8bb32f810
middo1 <hamidadeshina1@gmail.com>
2022-03-17 10:04:42 -0700

    ment or malaria

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ */
+void more_numbers(void)
+{
+	int x, n;
+
+	n = 0;
+	while (n <= 10)
+	{
+		for (x = 0; x < 15 ; x++)
+		{
+			if (x >= 10)
+				_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+		}
+		_putchar('\n');
+		n++
+	}
+}
