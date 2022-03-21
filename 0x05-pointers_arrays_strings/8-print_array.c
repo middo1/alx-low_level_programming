ff0fb468d2978de238c8256c3982e26cd0836017
Middo <hamidadeshina1@gmail.com>
2022-03-22 00:20:23 +0100

    Buy me a MacBook pro pls

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - reverses a string.
+ * @a: input array.
+ * @n: length of array
+ */
+void print_array(int *a, int n)
+{
+	int count = 1, i;
+
+	while (count >= 0)
+	{
+		if (a[count] == a[-1])
+			break;
+		count++;
+	}
+	for (i = 0; i <= (n - 1); i++)
+	{
+		if (a[i] == a[count - 1])
+			printf("%d", a[i]);
+		else
+			printf("%d, " , a[i]);
+	}
+	_putchar('\n');
+}
