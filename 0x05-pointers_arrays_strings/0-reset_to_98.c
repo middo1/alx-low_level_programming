ad6383a5f4b21ffd9c2d34078aac2a8f4b6f8290
middo1 <hamidadeshina1@gmail.com>
2022-03-21 00:23:39 -0700

    starting things fresh

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @*n: a pointer and it is used to reset the value of the number to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers, arrays and array of char(string)
