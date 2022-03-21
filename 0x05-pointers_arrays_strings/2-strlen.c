f1e9977b915bdc3a6523ed39e182e0d684417387
middo1 <hamidadeshina1@gmail.com>
2022-03-21 00:48:27 -0700

    starting things fresh

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * @s:str that its char are to be counted
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		return (i);
+	}
+	return (0);
+}
