fd1911c8fde9f86c5b01edfb6d8fe91a32c4474c
middo1 <hamidadeshina1@gmail.com>
2022-03-21 00:25:37 -0700

    starting things fresh

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - Entry point
- * @*n: a pointer and it is used to reset the value of the number to 98
+ * @n: a pointer and it is used to reset the value of the number to 98
  */
 
 void reset_to_98(int *n)
