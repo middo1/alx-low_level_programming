5c9e88967d2a0320363ece66a3a258de2b71158e
Middo <hamidadeshina1@gmail.com>
2022-03-21 21:56:07 +0100

    if you're reading this send me some money I am very broke rn

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - reverses a string.
+ * @str: input string.
+ */
+void puts2(char *str)
+{
+	int count = 0, i;
+	char *s;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+	s = str;
+
+	for (i = 0; i <= (count - 1); i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(*(s + i));
+		}	
+	}
+}
