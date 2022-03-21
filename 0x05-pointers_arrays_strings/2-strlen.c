66b64d59e22e8c6ffc3150e4461e1bd4f3ee2e55
middo1 <hamidadeshina1@gmail.com>
2022-03-21 00:55:03 -0700

    starting things fresh

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,6 +2,7 @@
 /**
  * _strlen - Entry point
  * @s:str that its char are to be counted
+ * return:the len of the string
  */
 int _strlen(char *s)
 {
@@ -10,5 +11,7 @@ int _strlen(char *s)
 	for (i = 0; s[i] != '\0'; ++i)
 	{
 		return (i);
+		_putchar(i + '0');
 	}
+	return (i);
 }
