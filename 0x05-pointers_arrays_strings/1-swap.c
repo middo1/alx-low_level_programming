0d5db7be25369bbf87bef909123436ff48f85aab
middo1 <hamidadeshina1@gmail.com>
2022-03-21 00:32:11 -0700

    starting things fresh

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Entry point
+ * @a:first int to be swapped
+ * @b:second int to be swapped
+ */
+void swap_int(int *a,int *a)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
