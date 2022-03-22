c00f58b0738c8b3b9c3f19895713dc86addd78b5
Middo <hamidadeshina1@gmail.com>
2022-03-22 01:36:50 +0100

    Help my career by buying me a MacBook pro

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_array - reverses a string.
+ * _strcpy - copies a string
  * @dest: to be copied to
  * @src: to be copied from
  * Return: char *dest
@@ -8,10 +8,11 @@
 char *_strcpy(char *dest, char *src)
 {
 	int n = 0;
+
 	while (n >= 0)
 	{
 		*(dest + n) = *(src + n);
-		if (*(dest +n) == '\0')
+		if (*(dest + n) == '\0')
 			break;
 		n++;
 	}
