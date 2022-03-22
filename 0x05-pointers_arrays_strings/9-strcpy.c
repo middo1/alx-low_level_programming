e60086a1feda19f448727e638e269898917e3b19
Middo <hamidadeshina1@gmail.com>
2022-03-22 01:32:32 +0100

    Buy me a MacBook pro today

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - reverses a string.
+ * @dest: to be copied to
+ * @src: to be copied from
+ * Return: char *dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+	while (n >= 0)
+	{
+		*(dest + n) = *(src + n);
+		if (*(dest +n) == '\0')
+			break;
+		n++;
+	}
+	return (dest);
+}
