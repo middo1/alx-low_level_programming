3fc002415eb2d91a341a04ab7c61dc568530b60a
middo1 <hamidadeshina1@gmail.com>
2022-03-23 00:21:08 -0700

    buy me a macbook pro pls

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest:first string
+ * @src:second string
+ * Return:concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; ++i);
+	for (j = 0; src[j] != '\0'; ++j, ++i)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (*dest);
+}
