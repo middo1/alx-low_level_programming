daa4d4daed82c59e3e86ed61ed4ddcb5dc3ed45f
Middo <hamidadeshina1@gmail.com>
2022-03-28 14:42:49 +0100

    buy me a macbook pro pls

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,22 @@
-#include
+#include "main.h"
+/**
+ * _strchr - entry point
+ * @s: pointe to string
+ * @c: character
+ * Return: first occurrence of the char c
+ */
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+	char *tmp;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			tmp = (s + i);
+			return (tmp);
+		}
+	}
+	return (NULL);
+}
