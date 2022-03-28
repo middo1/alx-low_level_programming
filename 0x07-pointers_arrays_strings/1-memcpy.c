e2d65776764a8b43bc16b59a6c31073f49f55a1b
middo1 <hamidadeshina1@gmail.com>
2022-03-28 01:10:04 -0700

    macbook pro needed

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _memcpy - Entry point
+ * @dest: destination of copy
+ * @src: source of copy
+ * @n: number of bytes to be copied
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	char *ptr;
+
+	
