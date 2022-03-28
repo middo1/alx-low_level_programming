496ba5f9a3daabd7ad66c1dbf081f657a9637628
Middo <hamidadeshina1@gmail.com>
2022-03-28 13:46:53 +0100

    buy me a macbook pro pls

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
-	char *csrc = (char *)src;
-	char *cdest = (char *)dest;
+unsigned int i;
 
 	for (i = 0 ; i < n ; i++)
 	{
-		cdest[i] = csrc[i];
+		*(dest + i) = *(src + i);
 	}
 	return (dest);
 }
