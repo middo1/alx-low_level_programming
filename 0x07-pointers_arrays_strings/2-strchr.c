a6b82ad6a225b9f3acfae9c5e9cc6efce77b8bf4
Middo <hamidadeshina1@gmail.com>
2022-03-28 15:18:07 +0100

    buy me a macbook pro pls

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,14 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	for ( ; *(s + i) != '\0'; i++)
+	for (; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
 		{
 			return (s + i);
 		}
 	}
-	if (*(s + 1) == c)
+	if (*(s + i) == c)
 		return (s + i);
 	return ('\0');
 }
