608d77fc4f58d9b6eb1561dbe974ddbe8ef6e1e2
middo1 <hamidadeshina1@gmail.com>
2022-03-29 23:42:19 -0700

    nepa don show love today

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - Entry point
+ * @s: pointer to string
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*(s + 1) != '\0')
+	{
+		_puts_recursion(s + 1);
+	} else
+		_putchar('\n');
+}
