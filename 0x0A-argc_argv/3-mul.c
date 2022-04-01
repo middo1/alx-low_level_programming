a73ffb770855efc1a55c1cd8a4e458d2e36d210f
middo1 <hamidadeshina1@gmail.com>
2022-04-01 12:14:02 -0700

    gimme macbook (blow powder)

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,9 @@ int main(int argc, char **argv)
 	{
 		printf("Error\n");
 		return (1);
-	} else 
+	} else
 	{
-		printf("%d\n", atoi(*(argv + 1)) * atoi(*(argv +2)));
+		printf("%d\n", atoi(*(argv + 1)) * atoi(*(argv + 2)));
 		return (0);
 	}
 }
